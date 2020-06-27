f1cae1525c9df3b1dc2c9706d9d90218670ef242
Nima shams <n.feyzshams>
2020-06-27 10:50:41 +0430

    add a detalis for component Hero navbar sidebar disecart sefult

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import Hero from '../components/Hero';
+import defultBcg from '../images/defaultBcg.jpeg'
+import { Link } from 'react-router-dom';
 export default function Default() {
     return (
         <>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-            <h1>hello from Default</h1>
-
+            <Hero img={defultBcg} title="404" max="true">
+                <h2 className="text-uppercase">page not found</h2>
+                <Link to="/" className="main-link"
+                 style={{marginTop:"2rem"}}>
+                    return Home
+                </Link>
+            </Hero>
         </>
     )
 }
