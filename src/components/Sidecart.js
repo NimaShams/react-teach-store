f505fdefdf1b6331a23a7d73e65c5b24b1c9a422
Nima shams <n.feyzshams>
2020-06-30 14:37:53 +0430

    add single product and save product in localStorage

--- src/components/Sidecart.js
+++ src/components/Sidecart.js
@@ -5,7 +5,7 @@ export default function Sidecart() {
     return (
         <ProductConsumer>
             {value =>{
-                const { cartOpen,closeCart,openCart }=value;
+                const { cartOpen,closeCart }=value;
                 return <CartWrapper show={cartOpen} onClick={closeCart}>
                         <p>cart items</p>
                 </CartWrapper>
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
-export default function singleProductPage () {
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import SingleProductImg from '../images/singleProductBcg.jpeg';
+import { ProductConsumer } from '../context';
+export default function singleProductPage() {
     return (
         <>
-            <h1>hello from singleProductPage</h1>
+            <Hero img={SingleProductImg} title="Hello from single Product" />
+            <ProductConsumer>
+                {value => {
+                    const { singleProduct, addToCart, Loadnig }  = value;
+                    if (Loadnig) {
+                        return <h1>product Loading ....</h1>
+                    }
+                    const { company, description, id, price, title, image } = singleProduct
+                return<h3>{title}</h3>
+                }}
+            </ProductConsumer>
         </>
     )
 }
